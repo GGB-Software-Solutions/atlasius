2224b7faa21a0237302d5fff615a9217f094784b
Georgi Mavridis <georgimavridis21@gmail.com>
2022-09-17 23:59:39 +0300

    add speedy api routes

--- pages/api/speedy/offices.ts
+++ pages/api/speedy/offices.ts
@@ -1,8 +1,11 @@
 import { BULGARIA_COUNTRY_ID, fetcher } from "../../../src/speedy-api";
 
 export default async function handler(req, res) {
-  const data = await fetcher("/location/office", {
-    countryId: BULGARIA_COUNTRY_ID,
-  });
+  const body = req.body
+    ? JSON.parse(req.body)
+    : {
+        countryId: BULGARIA_COUNTRY_ID,
+      };
+  const data = await fetcher("/location/office", body);
   res.status(200).json(data.offices);
 }
---
+++ pages/api/speedy/streets.ts
@@ -0,0 +1,7 @@
+import { fetcher } from "../../../src/speedy-api";
+
+export default async function handler(req, res) {
+  const body = JSON.parse(req.body);
+  const data = await fetcher("/location/street", body);
+  res.status(200).json(data.streets);
+}
