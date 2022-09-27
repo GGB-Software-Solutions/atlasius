787c6ea24292ba39bf0a3703c495faaa780a7402
Georgi Mavridis <georgimavridis21@gmail.com>
2022-09-28 00:56:03 +0300

    move more things to common utils

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,12 @@
+import { darken, lighten } from "@mui/material/styles";
+
 export const getSelectOptions = (tsEnum) =>
   Object.keys(tsEnum)
     .filter((key) => Number.isNaN(Number(key)))
     .map((entry) => ({ id: tsEnum[entry], label: entry }));
+
+export const getBackgroundColor = (color: string, mode: string) =>
+  mode === "dark" ? darken(color, 0.6) : lighten(color, 0.6);
+
+export const getHoverBackgroundColor = (color: string, mode: string) =>
+  mode === "dark" ? darken(color, 0.5) : lighten(color, 0.5);
