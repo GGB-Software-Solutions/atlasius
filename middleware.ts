0e70dbc6ad86280ae5e9220b35c644801cd42e56
Georgi Mavridis <georgimavridis21@gmail.com>
2022-10-02 22:40:01 +0300

    add middleware

--- middleware.ts
+++ middleware.ts
@@ -6,12 +6,13 @@ export default withAuth({
     signIn: "/auth/login",
   },
   callbacks: {
-    async authorized({ req }) {
-      const token = await getToken({
+    async authorized({ req, token }) {
+      const token1 = await getToken({
         req,
         raw: true,
       });
-      return Boolean(token);
+      console.log("token1:", token1);
+      return Boolean(token1);
       // `/admin` requires admin role
       if (req.nextUrl.pathname === "/admin") {
         return token?.userRole === "admin";
@@ -21,3 +22,12 @@ export default withAuth({
     },
   },
 });
+
+// import { NextResponse, NextRequest } from "next/server";
+// export async function middleware(req: NextRequest, ev) {
+//   const { pathname } = req.nextUrl;
+//   if (pathname === "/auth/login") {
+//     return NextResponse.redirect("/");
+//   }
+//   return NextResponse.next();
+// }
