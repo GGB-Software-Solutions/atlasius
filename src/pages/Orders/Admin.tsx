de324e132025dd47f68ccc1d4658b747b0a30eff
Georgi Mavridis <georgimavridis21@gmail.com>
2022-11-07 13:32:39 +0200

    refresh orders list after phone update

--- src/pages/Orders/Admin.tsx
+++ src/pages/Orders/Admin.tsx
@@ -148,6 +148,7 @@ export default function Admin() {
     if (response.success) {
       setNotification({ type: "success", message: response.success });
       onSuccess(newRow);
+      mutate();
     } else {
       setNotification({ type: "error", message: response.error });
       onError();
